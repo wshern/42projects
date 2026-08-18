f8cba42584c8c1883a787351ca9101d0972eca82
wslim <imeva.ws@gmail.com>
2026-08-18 20:25:08 +0800

    completed ftsplit

--- core/libft_project/libft/ft_split.c
+++ core/libft_project/libft/ft_split.c
@@ -5,75 +5,83 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/16 01:23:47 by werlim            #+#    #+#             */
-/*   Updated: 2026/08/17 17:42:45 by werlim           ###   ########.fr       */
+/*   Created: 2026/08/18 16:48:37 by werlim            #+#    #+#             */
+/*   Updated: 2026/08/18 20:22:11 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	counter(char const *s, char c)
+static size_t	wordcounter(char const *s, char c)
 {
 	size_t	i;
-	size_t	counter;
+	size_t	count;
 
 	i = 0;
-	counter = 0;
+	count = 0;
 	if (s[0] == '\0')
-		return (0);
+		return (count);
 	while (s[i])
 	{
-		i++;
-		if (s[i] == c && s[i - 1] == c)
+		while (s[i] == c)
 			i++;
-		else if (s[i] == c)
-			counter++;
+		if (s[i] == '\0')
+			break ;
+		count++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
 	}
-	if (s[i - 1] != c)
-		counter++;
-	return (counter);
+	return (count);
+}
+
+static size_t	wordlen(char const *s, char c, const size_t i)
+{
+	size_t	len;
+
+	len = i;
+	while (s[len] && s[len] != c)
+		len++;
+	return (len - i);
 }
 
 static char	**f_arr(char const *s, char c, char **arr)
 {
-	int	i;
-	int	j;
-	int	start;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	j = 0;
-	while (s[i] == c)
-		i++;
-	start = i;
 	while (s[i])
 	{
-		i++;
-		if (s[i] == c && s[i - 1] != c)
+		while (s[i] == c)
+			i++;
+		if (s[i] == '\0')
+			break ;
+		arr[j++] = ft_substr(s, i, wordlen(s, c, i));
+		if (arr[j - 1] == NULL)
 		{
-			arr[j++] = ft_substr(s, start, (size_t)(i - start));
-			while (s[i] == c)
-				i++;
-			start = i;
+			while (j > 0)
+				free(arr[--j]);
+			free(arr);
+			return (NULL);
 		}
+		while (s[i] != c && s[i] != '\0')
+			i++;
 	}
-	if (s[i] == '\0' && s[i - 1] != c)
-		arr[j] = ft_substr(s, start, (size_t)(i - start));
 	return (arr);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	wordcount;
 	char	**arr;
+	size_t	wordcount;
 
-	if (!s)
-		return (NULL);
-	wordcount = counter(s, c);
+	wordcount = wordcounter(s, c);
 	arr = (char **)malloc(sizeof(char *) * (wordcount + 1));
 	if (arr == NULL)
 		return (NULL);
 	arr[wordcount] = NULL;
-	if (s[0] == '\0')
+	if (wordcount == 0)
 		return (arr);
 	else
 		arr = f_arr(s, c, arr);
@@ -82,19 +90,16 @@ char	**ft_split(char const *s, char c)
 /*
 int main(void)
 {
-	char *s = "xxtestxworldxxx   crazyxxx";
+	char *s = "xxtestx xxxwor dlsxx ";
 	char c = 'x';
 	char **arr;
 	int i = 0;
 
 	arr = ft_split(s, c);
-	while (arr[i] != NULL)
-	{
-		printf("%s, %d\n", arr[i], i);
-		i++;
-	}
+	if (arr == NULL)
+		return (0);
 	while (arr[i])
-		free(arr[i++]);
+		printf("fmain - %s\n", arr[i++]);
 	free(arr);
 	return (0);
 }*/
