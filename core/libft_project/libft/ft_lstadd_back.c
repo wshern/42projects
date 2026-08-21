b60c73846f5af43c2a693cd1a53f0b02faa29362
wslim <imeva.ws@gmail.com>
2026-08-21 18:14:12 +0800

    update norm error

--- core/libft_project/libft/ft_lstadd_back.c
+++ core/libft_project/libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 17:17:36 by werlim            #+#    #+#             */
-/*   Updated: 2026/08/20 17:49:32 by werlim           ###   ########.fr       */
+/*   Updated: 2026/08/21 18:12:48 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *temp;
-	
+	t_list	*temp;
+
 	if (!lst || !new)
 		return ;
 	if (!*lst)
--- core/libft_project/libft/ft_lstclear.c
+++ core/libft_project/libft/ft_lstclear.c
@@ -6,16 +6,16 @@
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 18:10:20 by werlim            #+#    #+#             */
-/*   Updated: 2026/08/20 19:33:35 by werlim           ###   ########.fr       */
+/*   Updated: 2026/08/21 18:13:20 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*temp;
-	
+
 	if (!lst)
 		return ;
 	while (*lst != NULL)
