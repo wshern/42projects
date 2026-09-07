359ac2f6b48edf76c038d61434c4e4849d3c2363
wslim <imeva.ws@gmail.com>
2026-09-07 18:24:43 +0800

    start GNL

--- core/get_next_line/get_next_line.h
+++ core/get_next_line/get_next_line.h
@@ -1,12 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   getnextline.c                                      :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/04 16:17:49 by werlim            #+#    #+#             */
-/*   Updated: 2026/09/07 17:35:20 by werlim           ###   ########.fr       */
+/*   Created: 2026/09/07 18:20:54 by werlim            #+#    #+#             */
+/*   Updated: 2026/09/07 18:24:29 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+char	*get_nextline(int fd);
+
+#endif
