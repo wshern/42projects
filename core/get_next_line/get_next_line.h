c0bc751b43054cd6b773132913e3be5bb0f42d83
wslim <imeva.ws@gmail.com>
2026-09-08 17:12:57 +0800

    update gnl and .h

--- core/get_next_line/get_next_line.h
+++ core/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 18:20:54 by werlim            #+#    #+#             */
-/*   Updated: 2026/09/07 18:24:29 by werlim           ###   ########.fr       */
+/*   Updated: 2026/09/07 21:20:42 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
+# include <stdlib.h>
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
--- core/get_next_line/getnextline.c
+++ core/get_next_line/getnextline.c
@@ -6,7 +6,40 @@
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 16:17:49 by werlim            #+#    #+#             */
-/*   Updated: 2026/09/07 17:35:20 by werlim           ###   ########.fr       */
+/*   Updated: 2026/09/07 21:18:36 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int	read_nextline(int fd)
+{
+	char	*rd_buf;
+	int		rd_n;
+
+	rd_buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!rd_buf)
+		return (0);
+	rd_n = 1;
+	if (rd_n == 0)
+		return (0);
+	while (rd_n > 0)
+	{
+		rd_n = read(fd, rd_buf, BUFFER_SIZE);
+		if (rd_n == -1)
+			return ;
+		rd_buf[rd_n] = '\0';
+		buffer = gnl_strjoin(stash, rd_buf);
+		if (gnl_strchr(buffer) == 1)
+			break;
+	}
+	
+}
+
+char	*get_next_line(int fd)
+{
+	static char *stash = NULL;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+}
